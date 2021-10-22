ed7b043d8c2d4fe7d924431e18f5f2f49b1cfb2a
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-22 22:46:05 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sessions = require('express-session');
 const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
 
 // import self made library
 const routes = require('./routes');
@@ -29,9 +30,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 usePassport(app);
+app.use(flash());
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated;
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.warning_msg = req.flash('warning_msg');
   next();
 });
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,21 +19,32 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const {name, email, password, confirmPassword} = req.body;
+  const errors = [];
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都必填!'});
+  }
+  if (password !== confirmPassword) {
+    errors.push({  message: '密碼確認與密碼不一致!' })
+  }
+  if (errors.length) {
+    return res.render('register', {errors, name, email, password, confirmPassword});
+  }
+
   User.findOne({ email })
     .then(user => {
       if(user) {
-        console.log('User already exists.')
-        res.render('register', { name, email, password, confirmPassword });
-      } else {
-        return User.create({ name, email, password })
-          .then(() => res.redirect('/'))
-          .catch(err => console.log(err));
-      }
+        errors.push({ message: '此信箱已進行過註冊。'})
+        return res.render('register', { errors, name, email, password, confirmPassword });
+      } 
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
     })
 });
 
 router.get('/logout', (req, res) => {
   req.logout();
+  req.flash('success_msg', '成功登出!');
   res.redirect('/users/login');
 });
 

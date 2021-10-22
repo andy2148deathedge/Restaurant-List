7a37749b5ac0c6c76b728e426753a6ae64485d27
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-22 17:24:43 +0800

    feat: add user models

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 // import express & express.Router
 const express = require('express');
 const router = express.Router();
+const home = require('./modules/home');
+const restaurants = require('./modules/restaurants');
+const users = require('./modules/users');
 
 // routing
-const home = require('./modules/home');
 router.use('/', home);
-
-const restaurants = require('./modules/restaurants');
 router.use('/restaurants', restaurants);
+router.use('/users', users);
 // routing
 
 // 匯出 router 至 wrapper　層

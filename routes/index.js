a5c7642dd7ea564c0e9e65546cfe68c0f09a5508
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-22 19:18:59 +0800

    feat: add user authentication in restaurants and home routers

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,12 @@ const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
 const users = require('./modules/users');
 
+const { authenticator } = require('../middleware/auth');
+
 // routing
-router.use('/', home);
-router.use('/restaurants', restaurants);
+router.use('/restaurants', authenticator, restaurants);
 router.use('/users', users);
-// routing
+router.use('/', authenticator, home);
 
 // 匯出 router 至 wrapper　層
 module.exports = router;
